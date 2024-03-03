4791c3881309e07ee9742bfe1d1dd36538810a5c
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-03-03 18:35:06 +0100

    update

--- hrhouz-terraform/main.tf
+++ hrhouz-terraform/main.tf
@@ -1,27 +1,48 @@
-
-# Create an EC2 instance
-resource "aws_instance" "hrhouz-server" {
-  ami                  = "ami-02bfcfbf6fc7e8ce4"
-  instance_type        = var.instance-type
-  key_name             = var.keypair
-  security_groups      = [aws_security_group.hrhouz_sg.name]
-  iam_instance_profile = aws_iam_instance_profile.hr-houz.name
-
-  tags = merge(
-    var.tags,
-    {
-      Name = format("%s-ec2", var.name)
-    }
-  )
-}
-
-# Create an IAM instance profile and attach the EC2 role
-resource "aws_iam_instance_profile" "hr-houz" {
-  name = "hr-houz"
-  role = aws_iam_role.ec2_role.name
-}
-
 # Create an ECR repository
 resource "aws_ecr_repository" "hrhouz-ecr" {
   name = "hrhouz-ecr"
 }
+
+
+# Create an ECS cluster
+resource "aws_ecs_cluster" "hrhouz_cluster" {
+  name = "hrhouz-cluster"
+}
+
+# Define the Task Definition (replace with your container image details)
+resource "aws_ecs_task_definition" "hrhouz_task_definition" {
+  family = "hrhouz-task-definition"
+  cpu    = "256"
+  memory = "512"
+
+  
+  container_definitions = <<EOF
+[
+  {
+    "name": "hrhouz-container",
+    "image": "939895954199.dkr.ecr.us-west-1.amazonaws.com/hrhouz-ecr:latest",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ]
+  }
+]
+EOF
+}
+
+# Create the ECS Service
+resource "aws_ecs_service" "hrhouz_service" {
+  name            = "hrhouz-ecs-service"
+  cluster         = aws_ecs_cluster.hrhouz_cluster.arn
+  task_definition = aws_ecs_task_definition.hrhouz_task_definition.arn
+  launch_type     = "EC2"
+  desired_count   = 1
+}
+
+
+
+
+
--- hrhouz-terraform/roles.tf
+++ hrhouz-terraform/roles.tf
@@ -1,7 +1,5 @@
-# IAM Role for EC2 instance
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2_role"
-
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecsTaskExecutionRole"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -9,7 +7,7 @@ resource "aws_iam_role" "ec2_role" {
     {
       "Effect": "Allow",
       "Principal": {
-        "Service": "ec2.amazonaws.com"
+        "Service": "ecs-tasks.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
     }
@@ -18,11 +16,16 @@ resource "aws_iam_role" "ec2_role" {
 EOF
 }
 
-# Attach an IAM policy to the EC2 role
-resource "aws_iam_role_policy_attachment" "ec2_policy_attachment" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+resource "aws_iam_policy_attachment" "ecs_task_execution_policy_attachment" {
+  name       = "ecsTaskExecutionPolicyAttachment"
+  roles      = [aws_iam_role.ecs_task_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+output "ecs_task_execution_role_arn" {
+  value = aws_iam_role.ecs_task_execution_role.arn
+}
+
 
 # IAM Role for ECR
 resource "aws_iam_role" "ecr_role" {
