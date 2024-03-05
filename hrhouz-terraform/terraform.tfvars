e2f1c320a226eaee57c3e3ea8585c919acd30c0b
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-03-05 08:01:51 +0100

    update

--- hrhouz-terraform/terraform.tfvars
+++ hrhouz-terraform/terraform.tfvars
@@ -1,3 +1,4 @@
 region = "us-west-1"
 
 name = "hrhouz"
+
--- hrhouz-terraform/vars.tf
+++ hrhouz-terraform/vars.tf
@@ -5,5 +5,57 @@ variable "region" {
 
 variable "name" {
   type        = string
-  description = "default name"
+  description = "default name given resources"
+}
+
+variable "name_prefix" {
+  default = "hrhouz"
+}
+
+variable "az_count" {
+  default = "2"
+}
+
+variable "healthcheck_path" {
+  default = "/"
+}
+
+variable "fargate_cpu" {
+  default = "1024"
+}
+
+variable "fargate_memory" {
+  default = "2048"
+}
+
+variable "ecs_task_execution_role" {
+  default = "arn:aws:iam::939895954199:role/ecsTaskExecutionRole"
+}
+
+variable "ecs_autoscale_role" {
+  default = "arn:aws:iam::939895954199:role/ecsAutoscaleRole"
+}
+
+variable "min_capacity" {
+  default = "2"
+}
+
+variable "max_capacity" {
+  default = "5"
+}
+
+variable "container_port" {
+  default = "80"
+}
+
+variable "alb_protocol" {
+  default = "HTTP"
+}
+
+variable "balanced_container_name" {
+  default = "hrhouz-api"
+}
+
+variable "app_image" {
+  default = "nginx:latest"
 }
