395656c8136adf359f1d57a7da7f93c76fb4443d
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-03-05 19:22:01 +0100

    update

--- hrhouz-ecr-ec2/roles.tf
+++ hrhouz-ecr-ec2/roles.tf
@@ -25,27 +25,49 @@ resource "aws_iam_role_policy_attachment" "ecr-policy-attachment" {
 }
 
 
-# Create IAM Role for EC2 instance
-resource "aws_iam_role" "hrhouz-ec2-role" {
-  name               = "${var.name}-ec2-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+# IAM Role for SSM to Access EC2
+
+resource "aws_iam_role" "ssm_role" {
+  name               = "SSM_EC2_Access_Role"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ssm.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-# Create IAM Instance Profile
-resource "aws_iam_instance_profile" "hrhouz-instance-profile" {
-  name = "hrhouz-instance-profile"
-  role = aws_iam_role.hrhouz-ec2-role.name
+resource "aws_iam_role_policy_attachment" "ssm_role_attachment" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
+
+# IAM Role for EC2 Instances
+
+resource "aws_iam_role" "ec2_role" {
+  name               = "EC2_Instance_Role"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "EC2_Instance_Profile"
+  role = aws_iam_role.ec2_role.name
+}
+
