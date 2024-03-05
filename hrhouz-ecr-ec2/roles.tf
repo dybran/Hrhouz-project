7389cbdaccf256e91dbe92f5d9ecf9c998344a4c
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-03-05 19:26:12 +0100

    update

--- hrhouz-ecr-ec2/roles.tf
+++ hrhouz-ecr-ec2/roles.tf
@@ -66,8 +66,8 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "EC2_Instance_Profile"
+resource "aws_iam_instance_profile" "hrhouz-instance-profile" {
+  name = "hrhouz-Instance-Profile"
   role = aws_iam_role.ec2_role.name
 }
 
