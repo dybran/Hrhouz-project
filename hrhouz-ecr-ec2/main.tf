cebadec77f2695270a9e1546cf2cd8cbbce4590a
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-03-05 16:23:17 +0100

    update

--- hrhouz-ecr-ec2/main.tf
+++ hrhouz-ecr-ec2/main.tf
@@ -27,6 +27,7 @@ resource "aws_instance" "hrhouz-server" {
   ami                  = data.aws_ami.latest_ubuntu.id
   instance_type        = var.inst-type
   subnet_id            = aws_subnet.hrhouz-subnet.id
+  associate_public_ip_address = true
   key_name             = var.key-pair
   security_groups      = [aws_security_group.hrhouz-sg.id]
   iam_instance_profile = aws_iam_instance_profile.hrhouz-instance-profile.name
