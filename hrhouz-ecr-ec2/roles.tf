89a6c4af86256ae5a74b0498534076a711a73ef3
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-03-06 15:16:31 +0100

    update

--- hrhouz-ecr-ec2/roles.tf
+++ hrhouz-ecr-ec2/roles.tf
@@ -24,19 +24,48 @@ resource "aws_iam_role_policy_attachment" "ecr-policy-attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 }
 
-# IAM role for EC2 to pull from ECR and AWS SSM to connect to EC@
+# IAM role for EC2 to pull from ECR and AWS SSM to connect to EC2
 resource "aws_iam_role" "ec2_role" {
-  name               = "ec2-ecr-ssm-role"
+  name               = "ec2_ecr_ssm_role"
   assume_role_policy = jsonencode({
     Version   = "2012-10-17",
-    Statement = [{
-      Effect    = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      },
-      Action    = "sts:AssumeRole"
-    }]
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action    = "sts:AssumeRole"
+      }
+    ]
   })
+}
+
+
+resource "aws_iam_policy" "ssm_session_policy" {
+  name = "ec2_instance_ssm_session_policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssmmessages:StartSession",
+        "ssmmessages:SendCommand",
+        "ssmmessages:ReceiveCommandOutput"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm_session_access" {
+  role = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ssm_session_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_ecr_access" {
@@ -44,10 +73,7 @@ resource "aws_iam_role_policy_attachment" "ec2_ecr_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_ssm_access" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
+
 
 
 resource "aws_iam_instance_profile" "hrhouz-instance-profile" {
