64ccb584448a8729659db53d3d7b99788d0978b6
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-03-06 11:53:39 +0100

    update

--- hrhouz-ecr-ec2/roles.tf
+++ hrhouz-ecr-ec2/roles.tf
@@ -25,49 +25,44 @@ resource "aws_iam_role_policy_attachment" "ecr-policy-attachment" {
 }
 
 
-# IAM Role for SSM to Access EC2
+provider "aws" {
+  region = "your_aws_region"
+}
 
-resource "aws_iam_role" "ssm_role" {
-  name               = "SSM_EC2_Access_Role"
+resource "aws_iam_role" "ec2_role" {
+  name               = "ec2-ecr-ssm-role"
   assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "ssm.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      }
-    ]
+    Version   = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action    = "sts:AssumeRole"
+    }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_role_attachment" {
-  role       = aws_iam_role.ssm_role.name
+resource "aws_iam_role_policy_attachment" "ec2_ecr_access" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm_access" {
+  role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# IAM Role for EC2 Instances
-
-resource "aws_iam_role" "ec2_role" {
-  name               = "EC2_Instance_Role"
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "ec2.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      }
-    ]
-  })
+resource "aws_instance" "example_ec2" {
+  ami                    = "your_ami_id"
+  instance_type          = "your_instance_type"
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
+  tags = {
+    Name = "ExampleEC2Instance"
+  }
 }
 
 resource "aws_iam_instance_profile" "hrhouz-instance-profile" {
-  name = "hrhouz-Instance-Profile"
+  name = "hrhouz-instance-profile"
   role = aws_iam_role.ec2_role.name
 }
-
