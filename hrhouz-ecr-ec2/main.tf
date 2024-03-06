3322d0a4bd238d1046ada6faa44dc47ca8c9572f
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-03-06 16:19:09 +0100

    update

--- hrhouz-ecr-ec2/main.tf
+++ hrhouz-ecr-ec2/main.tf
@@ -30,6 +30,13 @@ resource "aws_instance" "hrhouz-server" {
   associate_public_ip_address = true
   key_name             = var.key-pair
   vpc_security_group_ids      = [aws_security_group.hrhouz-sg.id]
+
+  
+
+
+
+
+
   iam_instance_profile = aws_iam_instance_profile.hrhouz-instance-profile.name
 
   tags = {
---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# install docker and add user to the docker group
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+#install the latest version of docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo docker run hello-world
+
+# Add docker to user group
+sudo usermod -aG docker $USER
+
+# start and enable docker
+sudo systemctl start docker && sudo systemctl enable docker
+
+# Authenticate docker with ECR
+aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 939895954199.dkr.ecr.us-west-1.amazonaws.com/hrhouz-ecr
+
+# Docker pull and run
+docker pull 939895954199.dkr.ecr.us-west-1.amazonaws.com/hrhouz-ecr
+docker run -d -p 80:80 939895954199.dkr.ecr.us-west-1.amazonaws.com/hrhouz-ecr:latest
