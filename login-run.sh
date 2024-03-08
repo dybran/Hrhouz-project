ae1e0999a837a075adf0cdac8221782cf83d00b8
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-03-09 00:04:24 +0100

    update

--- login-run.sh
+++ login-run.sh
@@ -3,6 +3,7 @@
 
 ECR_URL=$1
 INSTANCE_IP=$2
+HRHOUZ_IMAGE=$3
 
 
 # Authenticate docker with ECR
@@ -15,7 +16,7 @@ docker pull ${ECR_URL}
 docker run --name parse-mongo-db -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=password -p 27017:27017 -d mongo
 
 # Run Parse server container
-docker run --name parse-server -e PARSE_SERVER_APPLICATION_ID=hrhouz -e PARSE_SERVER_MASTER_KEY=master_key -e PARSE_SERVER_JAVASCRIPT_KEY=javascript_key -e PARSE_SERVER_ALLOW_ORIGIN=* -e PARSE_SERVER_DATABASE_URI=mongodb://admin:password@mongo/parse_server?authSource=admin --link parse-mongo-db:mongo -p 1337:1337 -d parseplatform/parse-server --masterKeyIps "0.0.0.0/0"
+docker run --name parse-server -e PARSE_SERVER_APPLICATION_ID=hrhouz -e PARSE_SERVER_MASTER_KEY=master_key -e PARSE_SERVER_JAVASCRIPT_KEY=javascript_key -e PARSE_SERVER_ALLOW_ORIGIN=* -e PARSE_SERVER_DATABASE_URI=mongodb://admin:password@mongo/parse_server?authSource=admin --link parse-mongo-db:mongo -p 1337:1337 -d ${HRHOUZ_IMAGE} --masterKeyIps "0.0.0.0/0"
 
 # Run Parse dashboard container
 docker run --name parse-dashboard -e PARSE_DASHBOARD_APP_ID=hrhouz -e PARSE_DASHBOARD_MASTER_KEY=master_key -e PARSE_DASHBOARD_USER_ID=admin -e PARSE_DASHBOARD_USER_PASSWORD=password -e PARSE_DASHBOARD_ALLOW_INSECURE_HTTP=true -e PARSE_DASHBOARD_SERVER_URL=http://${INSTANCE_IP}:1337/parse --link parse-server:parse-server -p 4040:4040 -d parseplatform/parse-dashboard
