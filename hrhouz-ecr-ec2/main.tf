8262be8099bf0358c3c21accce6c1ea2192f6439
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-03-24 06:06:31 +0100

    update

--- hrhouz-ecr-ec2/main.tf
+++ hrhouz-ecr-ec2/main.tf
@@ -5,26 +5,9 @@ resource "aws_ecr_repository" "hrhouz-ecr" {
   force_delete         = true
 }
 
-# Find the latest Ubuntu AMI
-data "aws_ami" "latest_ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = [var.ami]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = [var.owner] # Canonical ID for Ubuntu AMIs
-}
-
 # Launch EC2 instance with IAM Role
 resource "aws_instance" "hrhouz-server" {
-  ami                  = data.aws_ami.latest_ubuntu.id
+  ami                  = var.ami
   instance_type        = var.inst-type
   subnet_id            = aws_subnet.hrhouz-subnet.id
   associate_public_ip_address = true
