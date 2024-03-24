d3c25c70250bc78ccc5fbc8356ac9654ae52a9fb
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-03-24 05:55:24 +0100

    update

--- hrhouz-ecr-ec2/outputs.tf
+++ hrhouz-ecr-ec2/outputs.tf
@@ -4,6 +4,6 @@ output "ecr_repo_url" {
   value = aws_ecr_repository.hrhouz-ecr.repository_url
 }
 
-output "instance_id" {
-  value = aws_instance.hrhouz-server.id
+output "instance_ip" {
+  value = aws_instance.hrhouz-server.public_ip
 }
